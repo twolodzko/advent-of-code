a161d9935f6d496853f41541ea56b487614f39e7
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2020-12-16 00:04:50 +0100

    Day 15 solved!

--- day-15.jl
+++ day-15.jl
@@ -6,7 +6,11 @@ function parse_input(string)
     return map(x -> parse(Int, x), split(string, ',', keepempty=false))
 end
 
+"""
+Direct implementation. Doesn't scale.
+"""
 function create_sequence(starting_sequence, turns)
+    starting_sequence = reverse(starting_sequence)
     previous = starting_sequence[1]
     history = starting_sequence[2:end]
     for _ in 1:(turns - length(starting_sequence))
@@ -15,35 +19,35 @@ function create_sequence(starting_sequence, turns)
         previous = isnothing(pos) ? 0 : pos
     end
     pushfirst!(history, previous)
-    return history
+    return history[1]
 end
 
-function static_memory_sequence(starting_sequence, turns)
+"""
+Use array as a lookup table since keys are integers and we know the
+upper bound. Access to array's elements is O(1).
+"""
+function sequence_memory(starting_sequence, turns)
     n = max(turns + 1, maximum(starting_sequence))
-    memory = zeros(Int32, n)
-    mask = Integer[]
+    memory = zeros(Int32, n) .- (2 * n)
 
-    for (i, prev) in enumerate(starting_sequence[2:end])
+    for (i, prev) in enumerate(starting_sequence[1:(end - 1)])
         memory[prev + 1] = i
-        push!(mask, prev + 1)
     end
 
-    prev = starting_sequence[1]
-    for _ in 1:(turns - length(starting_sequence))
+    prev = starting_sequence[end]
+    pos = 0
+
+    for i in length(starting_sequence):(turns - 1)
         pos = memory[prev + 1]
-        if pos == 0
-            push!(mask, prev + 1)
-        end
-        memory[prev + 1] = 0
-        memory[mask] .+= 1
-        prev = pos
+        memory[prev + 1] = i
+        prev = pos > 0 ? i - pos : 0
     end
     return prev
 end
 
 function part1(string, turns=2020)
-    starting_sequence = reverse(parse_input(string))
-    return static_memory_sequence(starting_sequence, turns)
+    starting_sequence = parse_input(string)
+    return sequence_memory(starting_sequence, turns)
 end
 
 @assert part1(example, 10) == 0
@@ -57,17 +61,17 @@ end
 
 part2(string) = part1(string, 30000000)
 
-# @assert part2("0,3,6") == 175594
-# @assert part2("1,3,2") == 2578
-# @assert part2("2,1,3") == 3544142
-# @assert part2("1,2,3") == 261214
-# @assert part2("2,3,1") == 6895259
-# @assert part2("3,2,1") == 18
-# @assert part2("3,1,2") == 362
+@assert part2("0,3,6") == 175594
+@assert part2("1,3,2") == 2578
+@assert part2("2,1,3") == 3544142
+@assert part2("1,2,3") == 261214
+@assert part2("2,3,1") == 6895259
+@assert part2("3,2,1") == 18
+@assert part2("3,1,2") == 362
 
 test = "0,13,16,17,1,10,6"
 println("Part 1: $(part1(test))")
-# println("Part 2: $(part2(test))")
+println("Part 2: $(part2(test))")
 
 @assert part1(test) == 276
-# @assert part2(test) == 4288986482164
+@assert part2(test) == 31916
