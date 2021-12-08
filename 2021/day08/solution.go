88754d22d6280486704aaeff02aa5b370e2dc40d
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2021-12-08 23:26:50 +0100

    Day 8 - simplify part 1, no part 2

--- 2021/day08/solution.go
+++ 2021/day08/solution.go
@@ -46,28 +46,101 @@ func parse(row string) (Row, error) {
 	return Row{strings.Fields(fields[0]), strings.Fields(fields[1])}, nil
 }
 
-func findObviousCandidates(fields []string) []int {
+func simpleMatches(rows []Row) int {
 	var lengths = map[int]int{
 		2: 1,
 		4: 4,
 		3: 7,
 		7: 8,
 	}
-	var candidates []int
-	for _, field := range fields {
-		if val, ok := lengths[len(field)]; ok {
-			candidates = append(candidates, val)
-		}
-	}
-	return candidates
-}
-
-func simpleMatches(rows []Row) int {
 	total := 0
 	for _, row := range rows {
-		total += len(findObviousCandidates(row.output))
+		for _, field := range row.output {
+			if _, ok := lengths[len(field)]; ok {
+				total += 1
+			}
+		}
 	}
 	return total
+}
+
+// ================================================
+
+func uniqueWords(row Row) []string {
+	words := make(map[string]bool)
+	for _, w := range row.patterns {
+		if _, ok := words[w]; !ok {
+			words[w] = true
+		}
+	}
+	for _, w := range row.output {
+		if _, ok := words[w]; !ok {
+			words[w] = true
+		}
+	}
+	var out []string
+	for w, _ := range words {
+		out = append(out, w)
+	}
+	return out
+}
+
+type Counter struct {
+	counts map[rune]int
+}
+
+func newCounter() *Counter {
+	counts := make(map[rune]int)
+	return &Counter{counts}
+}
+
+func (c *Counter) Add(v rune) {
+	if _, ok := c.counts[v]; ok {
+		c.counts[v] += 1
+	} else {
+		c.counts[v] = 1
+	}
+}
+
+func countChars(words []string) *Counter {
+	counter := newCounter()
+	for _, word := range words {
+		for _, r := range word {
+			counter.Add(r)
+		}
+	}
+	return counter
+}
+
+func referenceCounts() map[rune]int {
+	reference := countChars([]string{
+		"abcefg",
+		"cf",
+		"acdeg",
+		"acdfg",
+		"bcdf",
+		"abdfg",
+		"abdefg",
+		"acf",
+		"abcdefg",
+		"abcdf",
+	})
+	return reference.counts
+}
+
+func lifeSupportRating(row Row) int {
+	words := uniqueWords(row)
+	fmt.Println(words)
+	reference := referenceCounts()
+	for k, v := range reference {
+		fmt.Printf("%q => %d\n", k, v)
+	}
+	fmt.Println("=========")
+	counter := countChars(words)
+	for k, v := range counter.counts {
+		fmt.Printf("%q => %d\n", k, v)
+	}
+	return -1
 }
 
 func main() {
@@ -84,8 +157,8 @@ func main() {
 	result1 := simpleMatches(arr)
 	fmt.Printf("Puzzle 1: %v\n", result1)
 
-	// row, _ := parse("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
-	// fmt.Println(row)
+	row, _ := parse("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+	lifeSupportRating(row)
 
 	// result2 := lifeSupportRating(arr)
 	// fmt.Printf("Puzzle 2: %v\n", result2)
