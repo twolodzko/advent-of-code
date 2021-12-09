aea7f46b02cd75ca4d3c094caa2c3e84e49a0304
Timothy Wolodzko <twolodzko+git@gmail.com>
2021-12-09 09:35:18 +0100

    Day 8 - problem 2 is buggy

--- 2021/day08/solution.go
+++ 2021/day08/solution.go
@@ -114,136 +114,76 @@ func intersection(a, b string) []rune {
 	return common
 }
 
-func remove(arr []string, key string) []string {
-	for i, x := range arr {
-		if x == key {
-			switch i {
-			case 0:
-				return arr[1:]
-			case len(arr) - 1:
-				return arr[:len(arr)-1]
-			default:
-				return append(arr[:i], arr[i+1:]...)
-			}
-		}
-	}
-	return arr
-}
-
-func lifeSupportRating(row Row) int {
+func getMapping(row Row) map[int]string {
 	words := uniqueWords(row)
 
 	// 1, 4, 7, 8
 	candidates, _ := obviousCandidates(words)
-	for _, c := range candidates {
-		words = remove(words, c)
-	}
 
-	fmt.Println(candidates)
-	fmt.Println(words)
+	//  aaaa
+	// b    c
+	// b    c
+	//  dddd
+	// e    f
+	// e    f
+	//  dddd
 
-	// 6
-	for i := 0; i < len(words); i++ {
-		w := words[i]
-		if len(w) != 6 {
-			continue
-		}
-		c, ok := candidates[1]
-		if ok && len(intersection(c, w)) == 1 {
-			candidates[6] = w
-			words = remove(words, w)
-		} else {
-			c, ok := candidates[4]
-			if ok && len(intersection(c, w)) == 3 {
+	for _, w := range words {
+		if len(w) == 6 {
+			// 0, 6, 9
+			if c, ok := candidates[1]; ok && len(intersection(c, w)) == 1 {
 				candidates[6] = w
-				words = remove(words, w)
-			}
-		}
-	}
-
-	fmt.Println(candidates)
-	fmt.Println(words)
-
-	// 0
-	for i := 0; i < len(words); i++ {
-		w := words[i]
-		if len(w) != 6 {
-			continue
-		}
-		c, ok := candidates[8]
-		if ok && len(intersection(c, w)) == 6 {
-			c, ok := candidates[1]
-			if ok && len(intersection(c, w)) == 2 {
+			} else if c, ok := candidates[7]; ok && len(intersection(c, w)) == 2 {
+				candidates[6] = w
+			} else if c, ok := candidates[4]; ok && len(intersection(c, w)) == 4 {
+				candidates[9] = w
+			} else {
 				candidates[0] = w
-				words = remove(words, w)
 			}
 		} else {
-			c, ok := candidates[4]
-			if ok && len(intersection(c, w)) == 3 {
-				candidates[0] = w
-				words = remove(words, w)
+			// 2, 3, 5,
+			if c, ok := candidates[1]; ok && len(intersection(c, w)) == 2 {
+				candidates[3] = w
+			} else if c, ok := candidates[7]; ok && len(intersection(c, w)) == 3 {
+				candidates[3] = w
+			} else if c, ok := candidates[4]; ok && len(intersection(c, w)) == 3 {
+				candidates[5] = w
+			} else {
+				candidates[2] = w
 			}
 		}
 	}
+	return candidates
+}
 
-	fmt.Println(candidates)
-	fmt.Println(words)
-
-	// 9
-	for i := 0; i < len(words); i++ {
-		w := words[i]
-		if len(w) != 6 {
-			continue
-		}
-		c, ok := candidates[4]
-		if ok && len(intersection(c, w)) == 4 {
-			candidates[9] = w
-			words = remove(words, w)
+func decodeOutput(output []string, candidates map[int]string) []int {
+	var decoded []int
+	for _, field := range output {
+		for digit, pattern := range candidates {
+			if field == pattern {
+				decoded = append(decoded, digit)
+			}
 		}
 	}
+	return decoded
+}
 
-	fmt.Println(candidates)
-	fmt.Println(words)
-
-	// 3
-	for i := 0; i < len(words); i++ {
-		w := words[i]
-		if len(w) != 5 {
-			continue
+func lifeSupportRating(rows []Row) int {
+	total := 0
+	for _, row := range rows {
+		mapping := getMapping(row)
+		decoded := decodeOutput(row.output, mapping)
+		n := len(decoded)
+		dec := 1
+		for i := 0; i < n; i++ {
+			dec *= 10
 		}
-		c, ok := candidates[1]
-		if ok && len(intersection(c, w)) == 2 {
-			candidates[3] = w
-			words = remove(words, w)
-		}
-	}
-
-	fmt.Println(candidates)
-	fmt.Println(words)
-
-	// 5
-	for i := 0; i < len(words); i++ {
-		w := words[i]
-		if len(w) != 5 {
-			continue
-		}
-		c, ok := candidates[4]
-		if ok && len(intersection(c, w)) == 3 {
-			candidates[5] = w
-			words = remove(words, w)
+		for _, d := range decoded {
+			dec /= 10
+			total += d * dec
 		}
 	}
-
-	fmt.Println(candidates)
-
-	if len(words) > 1 {
-		log.Fatalf("invalid input: %v", words)
-	}
-
-	candidates[2] = words[0]
-
-	fmt.Println(candidates)
-	return -1
+	return total
 }
 
 func main() {
@@ -260,9 +200,10 @@ func main() {
 	result1 := simpleMatches(arr)
 	fmt.Printf("Puzzle 1: %v\n", result1)
 
-	row, _ := parse("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
-	lifeSupportRating(row)
+	// row, _ := parse("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+	// result2 := lifeSupportRating([]Row{row})
+	// fmt.Println(result2)
 
-	// result2 := lifeSupportRating(arr)
-	// fmt.Printf("Puzzle 2: %v\n", result2)
+	result2 := lifeSupportRating(arr)
+	fmt.Printf("Puzzle 2: %v\n", result2)
 }
