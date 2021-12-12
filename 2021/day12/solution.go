2ce4971cc1f26d39f30a85eed1da91cf833ad23d
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2021-12-12 17:43:12 +0100

    Day 12

--- 2021/day12/solution.go
+++ 2021/day12/solution.go
@@ -29,27 +29,32 @@ func readFile(filename string) (map[string][]string, error) {
 			return paths, fmt.Errorf("invalid input: %s", line)
 		}
 
-		if _, ok := paths[fields[0]]; !ok {
-			paths[fields[0]] = []string{}
-		}
-		paths[fields[0]] = append(paths[fields[0]], fields[1])
-
-		if fields[0] != "start" && fields[1] != "end" {
-			if _, ok := paths[fields[1]]; !ok {
-				paths[fields[1]] = []string{}
-			}
-			paths[fields[1]] = append(paths[fields[1]], fields[0])
-		}
-
+		paths = add(paths, fields[0], fields[1])
+		paths = add(paths, fields[1], fields[0])
 	}
 	err = scanner.Err()
 	return paths, err
+}
+
+func add(paths map[string][]string, from, to string) map[string][]string {
+	if from == "end" || to == "start" {
+		return paths
+	}
+	if _, ok := paths[from]; !ok {
+		paths[from] = []string{}
+	}
+	paths[from] = append(paths[from], to)
+	return paths
 }
 
 type Explorer struct {
 	path      string
 	explored  []string
 	corridors map[string][]string
+}
+
+func (e *Explorer) Done() bool {
+	return e.path == "end"
 }
 
 func IsLower(s string) bool {
@@ -61,33 +66,46 @@ func IsLower(s string) bool {
 	return true
 }
 
-func newExplorer(path string, explored []string, corridors map[string][]string) Explorer {
-	return Explorer{path, explored, corridors}
-}
-
-func (e *Explorer) NextSteps() []Explorer {
+func (e *Explorer) NextSteps(shouldSkip func([]string, string) bool) []Explorer {
 	var unexplored []Explorer
 	explored := append(e.explored, e.path)
 	for _, path := range e.corridors[e.path] {
-		if e.WasVisited(path) {
+		if IsLower(path) && shouldSkip(explored, path) {
 			continue
 		}
-		unexplored = append(unexplored, newExplorer(path, explored, e.corridors))
+		unexplored = append(unexplored, Explorer{path, explored, e.corridors})
 	}
 	return unexplored
 }
 
-func (e *Explorer) Done() bool {
-	return e.path == "end"
-}
-
-func (e *Explorer) WasVisited(path string) bool {
-	if !IsLower(path) {
-		return false
-	}
-	for _, x := range e.explored {
+func wasVisited(explored []string, path string) bool {
+	for _, x := range explored {
 		if path == x {
 			return true
+		}
+	}
+	return false
+}
+
+func invalidPath(explored []string, path string) bool {
+	counter := make(map[string]int)
+	for _, p := range explored {
+		if IsLower(p) {
+			if _, ok := counter[p]; !ok {
+				counter[p] = 1
+			} else {
+				counter[p] += 1
+			}
+		}
+	}
+	if count, ok := counter[path]; ok && count > 1 {
+		return true
+	}
+	if count, ok := counter[path]; ok && count == 1 {
+		for _, count := range counter {
+			if count > 1 {
+				return true
+			}
 		}
 	}
 	return false
@@ -114,22 +132,27 @@ func (q *Queue) HasNext() bool {
 	return len(q.items) > 0
 }
 
-func explore(paths map[string][]string) int {
+func explore(paths map[string][]string, once bool) int {
+	var skipFn func([]string, string) bool
+	if once {
+		skipFn = wasVisited
+	} else {
+		skipFn = invalidPath
+	}
+
 	var queue Queue
 	for _, path := range paths["start"] {
 		queue.Push([]Explorer{{path, []string{"start"}, paths}})
 	}
+
 	var found []Explorer
 	for queue.HasNext() {
 		explorer := queue.Pop()
 		if explorer.Done() {
 			found = append(found, explorer)
 		}
-		queue.Push(explorer.NextSteps())
+		queue.Push(explorer.NextSteps(skipFn))
 	}
-	// for _, e := range found {
-	// 	fmt.Printf("%v -> %v (%v)\n", e.explored, e.path, e.blocked)
-	// }
 	return len(found)
 }
 
@@ -144,9 +167,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	result1 := explore(paths)
+	result1 := explore(paths, true)
 	fmt.Printf("Puzzle 1: %v\n", result1)
 
-	// result2 := firstSynchronization(deepcopy(arr))
-	// fmt.Printf("Puzzle 2: %v\n", result2)
+	result2 := explore(paths, false)
+	fmt.Printf("Puzzle 2: %v\n", result2)
 }
