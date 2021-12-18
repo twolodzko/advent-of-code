ab06e7b161b6aec98983cd8ed54553af2c1b5e26
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2021-12-18 19:37:48 +0100

    Day 18 - vector

--- 2021/day18/solution.go
+++ 2021/day18/solution.go
@@ -3,20 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"math"
 	"os"
 	"strconv"
-	"strings"
-)
-
-type Action int
-
-const (
-	EXPLODE Action = iota
-	SPLIT
-	NOOP
 )
 
 func readFile(filename string) ([]string, error) {
@@ -40,138 +30,148 @@ func readFile(filename string) ([]string, error) {
 	return arr, err
 }
 
-type Pair [2]interface{}
+// func Split(num int) Pair {
+// 	left := int(math.Floor(float64(num) / 2))
+// 	right := int(math.Ceil(float64(num) / 2))
+// 	return Pair{left, right}
+// }
 
-func IsPair(obj interface{}) bool {
-	switch obj.(type) {
-	case Pair:
+const (
+	START int = -1
+	END       = -2
+)
+
+func isSeparator(r rune) bool {
+	switch r {
+	case '[', ',', ']':
 		return true
 	default:
 		return false
 	}
 }
 
+type Pair []int
+
 func (p Pair) String() string {
-	return fmt.Sprintf("[%v,%v]", p[0], p[1])
-}
-
-func (x Pair) Add(y Pair) Pair {
-	return Pair{x, y}
-}
-
-func getAction(obj interface{}, depth int) Action {
-
-	// pair, isPair := obj.(Pair)
-	// if !isPair {
-	// 	if num, ok := obj.(int); ok && num >= 10 {
-	// 		return SPLIT
-	// 	}
-	// 	return NOOP
-	// }
-
-	// if depth == 0 {
-	// 	leftmost := pair[0]
-	// 	getAction(pair[1])
-	// }
-
-	// switch obj := obj.(type) {
-	// case Pair:
-	// 	right := getAction(obj[0])
-	// 	left := getAction(obj[1])
-	// default:
-	// 	return NOOP
-	// }
-
-	// switch depth {
-	// case 0:
-	// case 2:
-	// 	return EXPLODE
-	// default:
-	// 	if !IsPair(obj) {
-	// 		return NOOP
-	// 	} else {
-	// 		return getAction()
-	// 	}
-	// }
-
-	return NOOP
-}
-
-func Explode(pair Pair) Pair {
-	return Pair{}
-}
-
-func Split(num int) Pair {
-	left := int(math.Floor(float64(num) / 2))
-	right := int(math.Ceil(float64(num) / 2))
-	return Pair{left, right}
-}
-
-type Reader struct {
-	*strings.Reader
-	Head rune
-}
-
-func (cr *Reader) NextRune() error {
-	r, _, err := cr.ReadRune()
-	if err != nil {
-		cr.Head = rune(0)
-		return err
-	}
-	cr.Head = r
-	return nil
-}
-
-func (r *Reader) ReadPair() (Pair, error) {
-	if r.Head != '[' {
-		return Pair{}, fmt.Errorf("not a pair start character: %v", r.Head)
-	}
-	err := r.NextRune()
-	if err != nil {
-		return Pair{}, err
-	}
-
-	var fields [2]interface{}
-	for i := 0; i < 2; i++ {
-		field, err := r.ReadField()
-		if err != nil {
-			return Pair{}, err
-		}
-		fields[i] = field
-		err = r.NextRune()
-		if err != nil && err != io.EOF {
-			return Pair{}, err
-		}
-	}
-	return Pair{fields[0], fields[1]}, nil
-}
-
-func (r *Reader) ReadField() (interface{}, error) {
-	var field []rune
-	for {
-		switch r.Head {
-		case ',', ']':
-			return strconv.Atoi(string(field))
-		case '[':
-			return r.ReadPair()
+	var s string
+	for i, v := range p {
+		switch v {
+		case START:
+			if i > 0 && p[i-1] != START {
+				s += ","
+			}
+			s += "["
+		case END:
+			s += "]"
 		default:
-			field = append(field, r.Head)
+			if i > 0 && p[i-1] != START {
+				s += ","
+			}
+			s += fmt.Sprintf("%v", v)
 		}
+	}
+	return s
+}
 
-		err := r.NextRune()
-		if err != nil {
-			return nil, err
-		}
+func max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
 	}
 }
 
-func ParsePair(str string) (Pair, error) {
-	reader := Reader{strings.NewReader(str), rune(0)}
-	err := reader.NextRune()
-	if err != nil {
-		return Pair{}, err
+func parse(str string) (Pair, error) {
+	var pattern []int
+	runes := []rune(str)
+	i := 0
+	for i < len(runes) {
+		switch runes[i] {
+		case '[':
+			pattern = append(pattern, START)
+		case ']':
+			pattern = append(pattern, END)
+		case ',':
+		default:
+			// number
+			var num []rune
+			for j := i; j < len(runes); j++ {
+				if isSeparator(runes[j]) {
+					break
+				}
+				num = append(num, runes[j])
+			}
+			i += max(len(num)-1, 0)
+			val, err := strconv.Atoi(string(num))
+			if err != nil {
+				return nil, err
+			}
+			pattern = append(pattern, val)
+		}
+		i++
 	}
-	return reader.ReadPair()
+	return pattern, nil
+}
+
+func reduce(p Pair) Pair {
+	depth := 0
+	i := 0
+	for i < len(p) {
+		fmt.Println(p)
+		if p[i] == START {
+			depth++
+		}
+		if p[i] == END {
+			depth--
+		}
+		if depth == 4 {
+			p = explode(p)
+			i = 0
+			depth = 0
+		}
+		if p[i] >= 10 {
+			p = split(p)
+			i = 0
+			depth = 0
+		}
+		i++
+	}
+	return p
+}
+
+func explode(p Pair) Pair {
+	for i := 0; i < len(p); i++ {
+		if p[i] > 0 && i+1 < len(p) && p[i+1] > 0 {
+			// leftmost pair
+			for j := 0; j < i; j++ {
+				if p[j] > 0 {
+					p[j] += p[i]
+				}
+			}
+			for j := i + 2; j < len(p); j++ {
+				if p[j] > 0 {
+					p[j] += p[i+1]
+				}
+			}
+			head := append(p[:i-1], 0)
+			return append(head, p[i+3:]...)
+		}
+	}
+	return p
+}
+
+func split(p Pair) Pair {
+	for i := 0; i < len(p); i++ {
+		if p[i] > 0 {
+			// leftmost regular number
+			x := float64(p[i]) / 2
+			left := int(math.Floor(x))
+			right := int(math.Ceil(x))
+			head := append(p[:i], []int{START, left, right, END}...)
+			return append(head, p[i+1:]...)
+		}
+	}
+	return p
 }
 
 func main() {
@@ -187,13 +187,16 @@ func main() {
 	}
 
 	for _, line := range lines {
-		pair, err := ParsePair(line)
+		pair, err := parse(line)
 		if err != nil {
 			log.Fatal(err)
 		}
 		if pair.String() != line {
 			log.Fatalf("Parsing error: %v != %v", pair, line)
 		}
+		fmt.Println(pair)
+		fmt.Println(reduce(pair))
+		fmt.Println()
 	}
 
 	// result1 := packet.VersionNumberSum()
