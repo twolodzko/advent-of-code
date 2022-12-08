a7d17fa9b2aba5ddd354595ae7f127ee8f477b7d
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-12-08 23:13:24 +0100

    Simplify

--- 2022/day-07/solution.sh
+++ 2022/day-07/solution.sh
@@ -15,20 +15,20 @@ cat "${INPUTFILE}" \
 | sed "s#cd /#cd $(pwd)/${MAIN}#" \
 | bash
 
-echo "Problem 1"
+# Problem 1
 
 paste \
-    <( du -b "${MAIN}" | sort -k2 | awk '{ print $2 "\t" $1 }' ) \
-    <( du -b "${MAIN}" | sort -k2 | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
-| awk '{ print $1 "\t" ($2 - (4096 * ($3 + 1))) }' \
-| awk '{ if ( $2 <= 100000 ) { print } }' \
-| awk '{ s += $2 } END { print s }'
+    <( du -b "${MAIN}" | cut -f1 ) \
+    <( du -b "${MAIN}" | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
+| awk '{ print ($1 - (4096 * ($2 + 1))) }' \
+| awk '{ if ($1 <= 100000) { print } }' \
+| awk '{ s += $1 } END { print s }'
 
-echo "Problem 2"
+# Problem 2
 
 paste \
-    <( du -b "${MAIN}" | sort -k2 | awk '{ print $2 "\t" $1 }' ) \
-    <( du -b "${MAIN}" | sort -k2 | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
+    <( du -b "${MAIN}" | cut -f1 ) \
+    <( du -b "${MAIN}" | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
     <( paste \
             <( du -bs "${MAIN}" ) \
             <( find "${MAIN}" -mindepth 1 -type d | wc -l ) \
@@ -36,8 +36,8 @@ paste \
         | xargs yes 2> /dev/null \
         | head -n "$( du -b "${MAIN}" | wc -l )" \
         ) \
-| awk '{ print ($2 - (4096 * ($3 + 1))) "\t" $4 }' \
-| awk '{ if ( $1 >= (30000000 - (70000000 - $2))) { print } }' \
+| awk '{ print ($1 - (4096 * ($2 + 1))) "\t" $3 }' \
+| awk '{ if ($1 >= (30000000 - (70000000 - $2))) { print } }' \
 | sort -k1 \
 | head -n1 \
 | cut -f1
