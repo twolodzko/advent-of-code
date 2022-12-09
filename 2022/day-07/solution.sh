34985c46ee81d8f0f3f9c19212034846446610b2
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-12-09 12:08:40 +0100

    Day 7 cleanup

--- 2022/day-07/solution.sh
+++ 2022/day-07/solution.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 set -e
 
-export MAIN=tmp
-export INPUTFILE="$1"
+WORKDIR=$(mktemp -d)
+INPUTFILE=$(realpath "$1")
 
-rm -r "${MAIN}"
-mkdir -p "${MAIN}"
+cd "${WORKDIR}"
 
 cat "${INPUTFILE}" \
 | grep -v '$ ls' \
 | sed -E 's/^dir ([A-Za-z0-9\._-]+)$/mkdir -p \1/' \
 | sed -E 's/^([0-9]+) ([A-Za-z0-9\._-]+)$/fallocate -l \1 \2/' \
 | sed -E 's/^\$ //' \
-| sed "s#cd /#cd $(pwd)/${MAIN}#" \
+| sed "s#cd /#cd ${WORKDIR}#" \
 | bash
+
+SIZES=$(mktemp)
+du -b "${WORKDIR}" > "${SIZES}"
 
 # Problem 1
 
 paste \
-    <( du -b "${MAIN}" | cut -f1 ) \
-    <( du -b "${MAIN}" | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
+    <( cut -f1 "${SIZES}" ) \
+    <( cut -f2 "${SIZES}" | xargs -i sh -c 'find {} -mindepth 1 -type d | wc -l' ) \
 | awk '{ print ($1 - (4096 * ($2 + 1))) }' \
 | awk '{ if ($1 <= 100000) { s += $1 } } END { print s }'
 
 # Problem 2
 
 paste \
-    <( du -b "${MAIN}" | cut -f1 ) \
-    <( du -b "${MAIN}" | cut -f2 | xargs -I % sh -c 'find % -mindepth 1 -type d | wc -l' ) \
+    <( cut -f1 "${SIZES}" ) \
+    <( cut -f2 "${SIZES}" | xargs -i sh -c 'find {} -mindepth 1 -type d | wc -l' ) \
     <( paste \
-            <( du -bs "${MAIN}" ) \
-            <( find "${MAIN}" -mindepth 1 -type d | wc -l ) \
+            <( du -bs "${WORKDIR}" ) \
+            <( find "${WORKDIR}" -mindepth 1 -type d | wc -l ) \
         | awk '{ print $1 - (($3 + 1) * 4096) }' \
         | xargs yes 2> /dev/null \
-        | head -n "$( du -b "${MAIN}" | wc -l )" \
-        ) \
+        | head -n "$( wc -l "${SIZES}" | cut -d' ' -f1 )" \
+    ) \
 | awk '{ print ($1 - (4096 * ($2 + 1))) "\t" $3 }' \
 | awk '{ if ($1 >= (30000000 - (70000000 - $2))) { print } }' \
 | sort -k1 \
 | head -n1 \
 | cut -f1
+
+rm -rf "${WORKDIR}" "$SIZES"
